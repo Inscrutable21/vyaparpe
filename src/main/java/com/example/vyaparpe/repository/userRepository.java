502b48493d0f4a964dae7b00fe688135695bafdb
Inscrutable21 <anandsinghoffical21@gmail.com>
2026-03-12 00:55:52 +0530

    upgraded the userRepo

--- src/main/java/com/example/vyaparpe/repository/userRepository.java
+++ src/main/java/com/example/vyaparpe/repository/userRepository.java
@@ -2,6 +2,8 @@ package com.example.vyaparpe.repository;
 
 import com.example.vyaparpe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface userRepository extends JpaRepository<User,Long> {
 }
