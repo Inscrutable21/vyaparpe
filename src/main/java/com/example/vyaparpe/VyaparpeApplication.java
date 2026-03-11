d9b9903ead49aee73460aa027fe550a210aee0e0
Inscrutable21 <anandsinghoffical21@gmail.com>
2026-03-11 16:56:31 +0530

    Setting up the project

---
+++ src/main/java/com/example/vyaparpe/VyaparpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.vyaparpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyaparpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyaparpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vyaparpe/VyaparpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vyaparpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyaparpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
